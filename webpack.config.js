4396ea0771df76e17e66befc2ebed9734045c63d
amodinho <u14284783@tuks.co.za>
2018-12-11 17:16:38 +0200

    split the config into parts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const merge = require("webpack-merge");
 
-module.exports = {
-    plugins: [
-        new htmlWebpackPlugin({
-            title: "Webpack starter"
-        })
-    ]
-}
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+    {
+        plugins: [
+            new htmlWebpackPlugin({
+                title: "Webpack starter"
+            })
+        ]
+    }
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        host:process.env.HOST,
+        port: process.env.PORT,
+    }),
+]);
+
+module.exports = mode => {
+   if(mode === "production"){
+       return merge(commonConfig,productionConfig,{mode});
+   }
+
+   return merge(commonConfig,developmentConfig,{mode});
+};
